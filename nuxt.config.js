d44699350d4dcb55f90d9adf2d2d12b4d4d73981
Thang Tran <thangtd1993@gmail.com>
2023-01-21 19:49:10 +0700

    base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  axios: {
+    baseURL: 'https://nammoi.viecviet.vn',
+  },
+
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
